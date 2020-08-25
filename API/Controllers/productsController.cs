9a0389c00f91b349391dc457b70eecf6f8691ab8
Ahassnat <alhassnatabdallah@gmail.com>
2020-08-25 21:09:08 +0300

    Intial Create

---
+++ API/Controllers/productsController.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class productsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public productsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> getProducts()
+        {
+            var products =await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> getProduct(int id)
+        {
+            return  await _context.Products.FindAsync(id);
+        }
+    }
+}
