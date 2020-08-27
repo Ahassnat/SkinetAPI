54c3d9dfdf7f65b42db276795d9636a4457ec9e6
Ahassnat <alhassnatabdallah@gmail.com>
2020-08-28 01:17:43 +0300

    3.9. Adding Seed data

--- API/Program.cs
+++ API/Program.cs
@@ -18,8 +18,10 @@ namespace API
         {
             CreateHostBuilder(args).Build().Run();
         }*/
-        // Apply any migration and create the DB if doesnt exist 
-        public static async Task Main(string[] args)
+        //
+       
+#region  Apply any migration and create the DB if doesnt exist 
+ public static async Task Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
             using (var scope = host.Services.CreateScope())
@@ -30,6 +32,7 @@ namespace API
                 {
                     var context = services.GetRequiredService<StoreContext>();
                     await context.Database.MigrateAsync();
+                    await StoreContextSeed.SeedAsync(context, loggerFactory);
                 }
                 catch(Exception ex)
                 {
@@ -40,7 +43,7 @@ namespace API
 
             host.Run();
         }
-
+#endregion
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
