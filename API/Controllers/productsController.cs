66fd28e36f9900f6292bc5b67a9edd961e17c00c
Ahassnat <alhassnatabdallah@gmail.com>
2020-09-04 19:45:32 +0300

    4.13. Configuring AutoMapper profiles

--- API/Controllers/productsController.cs
+++ API/Controllers/productsController.cs
@@ -34,20 +34,21 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<ProductToReturnDto>>> GetProducts()
+        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
         {
             var spec = new ProductsWithBrandsAndTypeSpecification();
             var products = await _productsRepo.ListAsync(spec);
-            return products.Select(product => new ProductToReturnDto 
-            {
-                Id=product.Id,
-                Name = product.Name,
-                Description = product.Description,
-                Price = product.Price,
-                PictureUrl = product.PictureUrl,
-                ProductBrand = product.ProductBrand.Name,
-                ProductType = product.ProductType.Name
-            }).ToList();
+            return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>> (products));
+            // return products.Select(product => new ProductToReturnDto 
+            // {
+            //     Id=product.Id,
+            //     Name = product.Name,
+            //     Description = product.Description,
+            //     Price = product.Price,
+            //     PictureUrl = product.PictureUrl,
+            //     ProductBrand = product.ProductBrand.Name,
+            //     ProductType = product.ProductType.Name
+            // }).ToList();
         }
 
         [HttpGet("{id}")]
